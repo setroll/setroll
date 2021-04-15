33d66434463313442fed874726b20228ccb6331a
setroll <redsvitarain@gmail.com>
2021-04-15 16:00:24 +0400

    master

--- gdx-game/src/com/mycompany/mygame/MyGdxGame.java
+++ gdx-game/src/com/mycompany/mygame/MyGdxGame.java
@@ -1,52 +1,84 @@
 package com.mycompany.mygame;
 
-import com.badlogic.gdx.*;
-import com.badlogic.gdx.graphics.*;
-import com.badlogic.gdx.graphics.g2d.*;
-import java.util.Vector;
 
-public class MyGdxGame implements ApplicationListener
-{
-	Texture texture; 
-	Texture head; 
-	Texture apple;
-	SpriteBatch batch;
-Vector gg;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+
+/**
+ * See: http://blog.xoppa.com/basic-3d-using-libgdx-2/
+ * @author Xoppa
+ */
+public class MyGdxGame implements ApplicationListener { 
+	public Environment enviroment;
+	public PerspectiveCamera cam;
+	public ModelBatch modelBatch;
+	public Model model;
+	public ModelInstance instance;
+	public CameraInputController camController;
 	@Override
-	public void create()
-	{
-		texture = new Texture(Gdx.files.internal("android.jpg"));
-		batch = new SpriteBatch();
+	public void create() {
+		enviroment = new Environment();
+		enviroment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+		enviroment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+
+		modelBatch = new ModelBatch();
+
+		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		cam.position.set(10f, 10f, 10f);
+		cam.lookAt(0,0,0);
+		cam.near = 1f;
+		cam.far = 300f;
+		cam.update();
+
+        ModelBuilder modelBuilder = new ModelBuilder();
+        model = modelBuilder.createBox(5f, 5f, 5f, 
+									   new Material(ColorAttribute.createDiffuse(Color.BLUE)),
+									   Usage.Position | Usage.Normal);
+        instance = new ModelInstance(model);  
+		camController = new CameraInputController(cam);
+        Gdx.input.setInputProcessor(camController);
+		
 	}
 
 	@Override
-	public void render()
-	{        
-	    Gdx.gl.glClearColor(1, 1, 1, 1);
-	    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(texture, Gdx.graphics.getWidth() / 4, 0, 
-				   Gdx.graphics.getWidth() / 2, Gdx.graphics.getWidth() / 2);
-		batch.end();
+	public void render() {
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+		camController.update();
+        modelBatch.begin(cam);
+        modelBatch.render(instance, enviroment);
+        modelBatch.end();
 	}
 
 	@Override
-	public void dispose()
-	{
+	public void dispose() {
+		modelBatch.dispose();
+		model.dispose();
 	}
 
 	@Override
-	public void resize(int width, int height)
-	{
+	public void resize(int width, int height) {
 	}
 
 	@Override
-	public void pause()
-	{
+	public void pause() {
 	}
 
 	@Override
-	public void resume()
-	{
+	public void resume() {
 	}
 }
+	
